e914e1d4985f09df9430dddf5f45922c42ec4ee1
Alvaro Sainz-Pardo <alvarosainzpardo@gmail.com>
2018-08-29 10:42:19 +0200

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,7 +10,18 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @word_with_guesses = '-' * word.size
+    @guesses = ""
+    @wrong_guesses = ""
+    @check_win_or_lose = :play
+    @bad_attempts = 0
   end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_reader :word_with_guesses
+  attr_reader :check_win_or_lose
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
@@ -22,6 +33,31 @@ class HangpersonGame
     Net::HTTP.new('watchout4snakes.com').start { |http|
       return http.post(uri, "").body
     }
+  end
+
+  def guess(letter)
+    raise ArgumentError, 'Argument is nil' if letter == nil
+    raise ArgumentError, 'Argument is empty' if letter.empty?
+    raise ArgumentError, 'Argument is not a letter' unless letter =~ /[a-zA-Z]/
+    return false if @check_win_or_lose != :play
+    return false if @guesses.downcase.include? letter.downcase
+    return false if @wrong_guesses.downcase.include? letter.downcase
+    if @word.downcase.include? letter.downcase
+      indexes = []
+      current_index = -1
+      while current_index = @word.downcase.index(letter.downcase, current_index + 1)
+        indexes << current_index
+      end
+      @guesses << letter
+      indexes.each { |index| @word_with_guesses[index] = @word[index] }
+      @check_win_or_lose = :win if @word_with_guesses.downcase == @word.downcase
+      return true
+    else
+      @wrong_guesses << letter
+      @bad_attempts += 1
+      @check_win_or_lose = :lose if @bad_attempts == 7
+      return false
+    end
   end
 
 end
