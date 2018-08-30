7cdcfc1b42e42f10e3c64c52c669fcb0d25b00c7
Alvaro Sainz-Pardo <alvarosainzpardo@gmail.com>
2018-08-30 16:47:04 +0200

    New version

--- app.rb
+++ app.rb
@@ -40,17 +40,15 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if !letter.nil?
-      game_guesses = @game.guesses + @game.wrong_guesses
-      if game_guesses.include? letter
-        flash[:message] = 'You have already used that letter.'
-      else
-        begin
-          @game.guess(letter)
-          rescue
-            flash[:message] = 'Invalid guess.'
-        end
-      end
+    letter = '' if letter.nil?
+    game_guesses = @game.guesses + @game.wrong_guesses
+    if game_guesses.include? letter
+      flash[:message] = 'You have already used that letter.'
+    end
+    begin
+      @game.guess(letter)
+      rescue
+        flash[:message] = 'Invalid guess.'
     end
     redirect '/show'
   end
